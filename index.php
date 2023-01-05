0eb15b9abd16d5ef253c5177c120d1f848a943ed
Tom Loudon <tloudong@srv603.webhostingforstudents.com>
2023-01-05 16:45:26 -0500

    create color form

--- index.php
+++ index.php
@@ -9,5 +9,12 @@
 </head>
 <body>
     <h1>pick a color</h1>
+    <form method = "post" action = "confirm.php">
+        <label><input type="radio" value="red" name="color">Red</label><br>
+        <label><input type="radio" value="blue" name="color">Blue</label><br>
+        <label><input type="radio" value="yellow" name="color">Yellow</label><br>
+        <input type="submit" value="submit">
+
+    </form>
 </body>
 </html>
